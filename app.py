4678d940d51b046d4fb52c39b0e446a8edc1b220
lalida mangkornkanok <lalida.mkanok@gmail.com>
2025-08-28 20:29:46 +0900

    update code

--- app.py
+++ app.py
@@ -1,1 +1,14 @@
-# "test test"
+import streamlit as st
+import joblib
+
+# Load model
+#model = joblib.load("model.pkl")
+
+st.title("When AI Sees Litter App")
+
+# Example: simple input
+user_input = st.text_input("Enter some text or number:")
+
+#if st.button("Predict"):
+#prediction = model.predict([[float(user_input)]])
+#st.write(f"Prediction: {prediction}")
